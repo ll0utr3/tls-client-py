854f186e2609bcee7c2819819904ec54efb6420d
Alexis <lsfsdfsdf@fdsfds.de>
2025-04-11 23:31:41 +0200

    up

--- tls_client/__version__.py
+++ tls_client/__version__.py
@@ -6,6 +6,6 @@
 
 __title__ = "tls_client"
 __description__ = "Advanced Python HTTP Client with session closing"
-__version__ = "1.8.0"
+__version__ = "1.9.1"
 __author__ = "ll0utr3"  # fork from Florian
 __license__ = "MIT"
